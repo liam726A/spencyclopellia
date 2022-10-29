b7febb41bde8365203edfa8cd6c655c09d2b7a60
liam726A <114473839+liam726A@users.noreply.github.com>
2022-10-29 14:31:50 +0900

    add: db

--- index/admin.py
+++ index/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, Topic, Language, Library, Spell
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Topic)
+admin.site.register(Language)
+admin.site.register(Library)
+admin.site.register(Spell)
--- index/models.py
+++ index/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    name_ja = models.CharField(max_length=100)
+    color = models.CharField(max_length=7, default="#303967")
+    explanation = models.TextField(default="None")
+    
+    def __str__(self):
+        return self.name
+
+class Topic(models.Model):
+    category = models.ManyToManyField(Category)
+    name = models.CharField(max_length=100)
+    name_ja = models.CharField(max_length=100)
+    color = models.CharField(max_length=7, default="#303967")
+    explanation = models.TextField(default="None")
+    
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+    text = models.TextField()
+    color = models.CharField(max_length=7, default="#303967")
+    page_hp = models.CharField(max_length=200)
+    page_doc = models.CharField(max_length=200)
+    page_github = models.CharField(max_length=200)
+    explanation = models.TextField(default="None")
+    
+    def __str__(self):
+        return self.name
+
+class Library(models.Model):
+    language = models.ManyToManyField(Language)
+    name = models.CharField(max_length=100)
+    text = models.TextField()
+    color = models.CharField(max_length=7, default="#303967")
+    page_hp = models.CharField(max_length=200)
+    page_doc = models.CharField(max_length=200)
+    page_github = models.CharField(max_length=200)
+    explanation = models.TextField(default="None")
+    
+    def __str__(self):
+        return self.name
+
+class Spell(models.Model):
+    category = models.ManyToManyField(Category)
+    topic = models.ManyToManyField(Topic)
+    language = models.ManyToManyField(Language)
+    library = models.ManyToManyField(Library)
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=7, default="#303967")
+    example_in = models.TextField(default="None")
+    example_out = models.TextField(default="None")
+    reference = models.TextField(default="None")
+    explanation = models.TextField(default="None")
+    
+    def __str__(self):
+        return self.name
