deba6265b08197717bf50488b1b7102abb3dff9b
liam726A <114473839+liam726A@users.noreply.github.com>
2022-10-29 15:22:31 +0900

    add: list.html

--- index/urls.py
+++ index/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index_view, name='index'),
+]
--- index/views.py
+++ index/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from .models import Category, Topic, Language, Library, Spell
+
 # Create your views here.
+def index_view(request):
+    category_list = Category.objects.all()
+    topic_list = Topic.objects.all()
+    language_list = Language.objects.all()
+    library_list = Library.objects.all()
+    spell_list = Spell.objects.all()
+
+    return render(request, 'index/list.html', {'category_list': category_list, \
+                                                'topic_list': topic_list, \
+                                                'language_list': language_list, \
+                                                'library_list': library_list, \
+                                                'spell_list': spell_list
+                                                })
+
+
+
